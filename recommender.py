db1463f8f8101df6341ed70371e35edeffb2e108
Jacob Miguel Villa <jacob_villa@dlsu.edu.ph>
2024-03-21 23:53:00 +0800

    update: proper checking of allergens based on allergen:ingredient csv

--- recommender.py
+++ recommender.py
@@ -1,9 +1,14 @@
 import pandas as pd
+import ast
 
 # Load the datasets
 indian_food_df = pd.read_csv("indian_food.csv")
 good_ingredients_mood_df = pd.read_csv("ingredients_mood-good.csv")
 bad_ingredients_mood_df = pd.read_csv("ingredients_mood-bad.csv")
+allergen_df = pd.read_csv("allergen.csv")
+
+# Fixing allergen_df
+allergen_df['allergens'] = allergen_df['allergens'].apply(ast.literal_eval)
 
 
 # Example to filter based on mood to get relevant ingredients
@@ -45,90 +50,103 @@ def get_dishes_based_on_mood(mood):
 
     return positive_dishes, negative_dishes
 
+# Given a dataframe of dishes, filter out dishes that contain ingredients correlated to the allergens
+def filter_dishes_by_allergens(dishes, allergies, get_good_dishes=True):
+    bad_ingredients = []
+
+    for index, row in allergen_df.iterrows():
+        if row['allergens'] != []:
+            for allergen in row['allergens']:
+                if allergen in allergies:
+                    bad_ingredients.append(row['ingredients'])
+                    continue
+
+    # Drop duplicates from bad ingredients
+    bad_ingredients = list(set(bad_ingredients))
+    print(bad_ingredients)
+
+    # Given the bad ingredients, filter the dishes to remove dishes that contain any of the bad ingredients
+    filtered_dishes = pd.DataFrame(columns=dishes.columns)
+    for index, row in dishes.iterrows():
+        # Terrible implementation but it is what it is 
+        common_ingredients = [i for i in bad_ingredients if i in row["ingredients"].split(", ")]
+        if common_ingredients == [] and get_good_dishes:
+            filtered_dishes = pd.concat([filtered_dishes, dishes[dishes["name"] == row["name"]]])
+        elif not get_good_dishes:
+            filtered_dishes = pd.concat([filtered_dishes, dishes[dishes["name"] == row["name"]]])
+    
+    return filtered_dishes
 
 def recommend_dishes(mood, allergies, is_vegan, like_spicy):
-    # FIXME: Double check algo
-
     # Get relevant ingredients for the mood
     positive_dishes, negative_dishes = get_dishes_based_on_mood(mood)
-    positive_ingredients = good_ingredients_mood_df[
-        good_ingredients_mood_df[mood].notna()
-    ]
-    negative_ingredients = bad_ingredients_mood_df[
-        bad_ingredients_mood_df[mood].notna()
-    ]
+    positive_ingredients = good_ingredients_mood_df[good_ingredients_mood_df[mood].notna()]
+    negative_ingredients = bad_ingredients_mood_df[bad_ingredients_mood_df[mood].notna()]
+
+    # print(f"before filter: {negative_dishes}")
 
     # Exclude based on user restrictions
     if is_vegan:
         positive_dishes = positive_dishes[positive_dishes["diet"] == "vegetarian"]
-    for allergen in allergies:
-        positive_dishes = positive_dishes[
-            ~positive_dishes["ingredients"].str.contains(allergen, case=False)
-        ]
+    # for allergen in allergies:
+    #     positive_dishes = positive_dishes[~positive_dishes['ingredients'].str.contains(allergen, case=False)]
+    if allergies:
+        positive_dishes = filter_dishes_by_allergens(positive_dishes, allergies, get_good_dishes=True)
     if not like_spicy:
-        positive_dishes = positive_dishes[
-            ~positive_dishes["flavor_profile"].str.contains("spicy", case=False)
-        ]
+        positive_dishes = positive_dishes[~positive_dishes['flavor_profile'].str.contains("spicy", case=False)]
 
     if is_vegan:
         negative_dishes = negative_dishes[negative_dishes["diet"] == "non vegetarian"]
+    # if allergies:
+    #     allergen_mask = pd.Series(False, index=negative_dishes.index)
+    #     for allergen in allergies:
+    #         # This updates the mask to True for any row where the allergen is found
+    #         allergen_mask |= negative_dishes['ingredients'].str.contains(allergen, case=False)
+    #     negative_dishes = negative_dishes[allergen_mask]
     if allergies:
-        allergen_mask = pd.Series(False, index=negative_dishes.index)
-        for allergen in allergies:
-            # This updates the mask to True for any row where the allergen is found
-            allergen_mask |= negative_dishes["ingredients"].str.contains(
-                allergen, case=False
-            )
-        negative_dishes = negative_dishes[allergen_mask]
+        negative_dishes = filter_dishes_by_allergens(negative_dishes, allergies, get_good_dishes=False)
     if not like_spicy:
-        negative_dishes = negative_dishes[
-            negative_dishes["flavor_profile"].str.contains("spicy", case=False)
-        ]
+        negative_dishes = negative_dishes[negative_dishes['flavor_profile'].str.contains("spicy", case=False)]
 
-    # print(positive_dishes)
-    final_recom = positive_dishes.sample(
-        n=3
-    )  # For simplicity, randomly pick three satisfying all conditions
+    final_recom = positive_dishes.sample(n=3)  # For simplicity, randomly pick three satisfying all conditions
+    # final_recom = positive_dishes.head(3)
 
+    # print(f"after filter: {negative_dishes}")
+    # FIXME: Some checks here that are present in recommender.py are missing here
     negative_dishes_empty = negative_dishes.empty
     avoid_recom = pd.DataFrame()
-    # FIXME: Brute forced check of 3 or more
-    if not negative_dishes_empty and len(negative_dishes.index) >= 3:
-        avoid_recom = negative_dishes.sample(
-            n=3
-        )  # For simplicity, randomly pick three satisfying all conditions
+    if not negative_dishes_empty:
+        avoid_recom = negative_dishes.sample(n=3)  # For simplicity, randomly pick three satisfying all conditions
 
     # Check if final_recom is empty
     if final_recom.empty:
-        print(
-            "Sorry, I couldn't find any dishes satisfying your preferences and restrictions."
-        )
+        print("Sorry, I couldn't find any dishes satisfying your preferences and restrictions.")
     else:
-        print(
-            f"Based on your mood and preferences, I recommend trying the following dishes: "
-        )
+        print(f"Based on your mood and preferences, I recommend trying the following dishes: ")
         for index, row in final_recom.iterrows():
             region_string = ""
             state_string = ""
             flavor_string = ""
-            if row["region"] != "-1":
+            if row['region'] != "-1":
                 region_string = f" of the {row['region']} region"
-            if row["state"] != "-1":
+            if row['state'] != "-1":
                 state_string = f" from the {row['state']} state"
             # print(type(row['flavor_profile']))
-            if row["flavor_profile"] != "-1":
+            if row['flavor_profile'] != "-1":
                 flavor_string = f" is a {row['flavor_profile']} dish and"
-            print(
-                f"{row['name']}{state_string}{region_string}, which is a {row['course']} dish."
-            )
+            print(f"{row['name']}{state_string}{region_string}, which is a {row['course']} dish.")
             print(f"{row['name']}{flavor_string} is {row['diet']}.")
             print(f"{row['name']} contains the ingredients {row['ingredients']}.")
-            for ingredient in row["ingredients"].split(","):
+            for ingredient in row['ingredients'].split(","):
+                # print(ingredient)
                 ingredient = ingredient.strip()
+                # print(positive_ingredients)
+                # FIXME: Check if ingredient is really missing from positive_ingredients["ingredients"] or it's just a difference in capitalization
+                # FIXME: If it's the former, then why is it missing if we got our recommendation based on the ingredients?
                 if ingredient in positive_ingredients["ingredients"].values:
-                    ingredient_reason = positive_ingredients.loc[
-                        positive_ingredients["ingredients"] == ingredient, "Y Reason"
-                    ].iloc[0]
+                    # print(positive_ingredients.loc[positive_ingredients['ingredients'] == ingredient, "Y Reason"].iloc[0])
+                    ingredient_reason = positive_ingredients.loc[positive_ingredients['ingredients'] == ingredient, "Y Reason"].iloc[0]
+                    # print(ingredient_reason)
                     if isinstance(ingredient_reason, str):
                         print("  " + ingredient_reason)
 
@@ -137,35 +155,28 @@ def recommend_dishes(mood, allergies, is_vegan, like_spicy):
         # print("I couldn't find any dishes that you should avoid.")
         pass
     else:
-        print(
-            f"Based on your mood and preferences, I recommend avoiding the following dishes: "
-        )
-        # FIXME: On one occasion, the above printed but did not print anything below
+        print(f"Based on your mood and preferences, I recommend avoiding the following dishes: ")
         for index, row in avoid_recom.iterrows():
             region_string = ""
             state_string = ""
             flavor_string = ""
-            if row["region"] != "-1":
+            if row['region'] != "-1":
                 region_string = f" of the {row['region']} region"
-            if row["state"] != "-1":
+            if row['state'] != "-1":
                 state_string = f" from the {row['state']} state"
             # print(type(row['flavor_profile']))
-            if row["flavor_profile"] != "-1":
+            if row['flavor_profile'] != "-1":
                 flavor_string = f" is a {row['flavor_profile']} dish and"
-            print(
-                f"{row['name']}{state_string}{region_string}, which is a {row['course']} dish."
-            )
+            print(f"{row['name']}{state_string}{region_string}, which is a {row['course']} dish.")
             print(f"{row['name']}{flavor_string} is {row['diet']}.")
             print(f"{row['name']} contains the ingredients {row['ingredients']}.")
-            for ingredient in row["ingredients"].split(","):
+            for ingredient in row['ingredients'].split(","):
                 # print(ingredient)
                 ingredient = ingredient.strip()
                 # print(negative_ingredients)
                 if ingredient in negative_ingredients["ingredients"].values:
                     # print(negative_ingredients.loc[negative_ingredients['ingredients'] == ingredient, "N Reason"].iloc[0])
-                    ingredient_reason = negative_ingredients.loc[
-                        negative_ingredients["ingredients"] == ingredient, "X Reason"
-                    ].iloc[0]
+                    ingredient_reason = negative_ingredients.loc[negative_ingredients['ingredients'] == ingredient, "X Reason"].iloc[0]
                     # print(ingredient_reason)
                     if isinstance(ingredient_reason, str):
                         print("  " + ingredient_reason)
